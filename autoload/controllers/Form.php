e2fda1eac36a232aa85ad3f5ca9488dde8624328
cIRUSIis <nikitaspy1975@gmail.com>
2018-03-17 03:52:37 +0300

    Добавил почту. Работает.

--- autoload/controllers/Form.php
+++ autoload/controllers/Form.php
@@ -7,7 +7,23 @@ class Form {
         $data = $f3->get('POST');
         $validate = \models\FormAction::validate($f3, $data);
         if($validate[status]) {
+            $email = "shkolavizhivaniya2018@gmail.com";
+            $subject = "$data[fio] - Школа Выживания";
+            $message = "<p>ФИО: $data[fio]</p>";
+            $message .= "<p>Факультет: $data[fakultet]</p>";
+            $message .= "<p>Курс: $data[kurs]</p>";
+            $message .= "<p>Группа: $data[group]</p>";
+            $message .= "<p>Телефон: $data[phone]</p>";
+            $message .= "<p>Ссылка VK: $data[vkLink]</p>";
+            $message .= "<p>Опыт походов: $data[experience]</p>";
+            $message .= "<p>Другой полезный опыт: $data[otherExperience]</p>";
+            $message .= "<p>Имеющийся инвентарь: $data[inventoryAvailiable]</p>";
+            $message .= "<p>Уровень физической подготовки (1-10): $data[fitnessLevel]</p>";
+            $message .= "<p>Болезни и травмы: $data[diseases]</p>";
+            $message .= "<p>Аллергии: $data[allergies]</p>";
+            $message .= "<p>Самое дальнее путешествие: $data[yourTrip]</p>";
             \models\FormAction::record($f3, $data);
+            \models\Mail::mailTo($email, $subject, $message);
             \Views\Render::formDoneRender($f3);
         } else {
             $f3->set('data', $data);
---
+++ autoload/models/Mail.php
@@ -0,0 +1,15 @@
+<?php
+namespace models;
+class Mail {
+    public static function mailTo($email, $subject, $message)
+    {
+		$subject = "Школа Выживания 2018 :: ITC - $subject";
+        $message = "<html><body>$message</body></html>"; 	
+        
+        $header = "From: Школа Выживания 2018 :: ITC <no-reply@the-center.it>\r\n"; 
+		$header .= "Reply-To: no-reply@the-center.it\r\n"; 
+        $header .= "Content-Type: text/html; charset=utf-8\r\n";
+        
+        return mail($email,$subject,$message,$header);
+    }
+}
