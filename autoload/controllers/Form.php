74e1a5ee7aa19e535d2fa97aeb1ee27c0400ea4f
wanku_x <wanku@bk.ru>
2018-03-19 15:41:01 +0300

    Create function createMessage()

--- autoload/controllers/Form.php
+++ autoload/controllers/Form.php
@@ -9,25 +9,13 @@ class Form {
         if($validate[status]) {
             $email = "shkolavizhivaniya2018@gmail.com";
             $subject = "$data[fio] - Школа Выживания";
-            $message = "<p>ФИО: $data[fio]</p>";
-            $message .= "<p>Факультет: $data[fakultet]</p>";
-            $message .= "<p>Курс: $data[kurs]</p>";
-            $message .= "<p>Группа: $data[group]</p>";
-            $message .= "<p>Телефон: $data[phone]</p>";
-            $message .= "<p>Ссылка VK: $data[vkLink]</p>";
-            $message .= "<p>Опыт походов: $data[experience]</p>";
-            $message .= "<p>Другой полезный опыт: $data[otherExperience]</p>";
-            $message .= "<p>Имеющийся инвентарь: $data[inventoryAvailiable]</p>";
-            $message .= "<p>Уровень физической подготовки (1-10): $data[fitnessLevel]</p>";
-            $message .= "<p>Болезни и травмы: $data[diseases]</p>";
-            $message .= "<p>Аллергии: $data[allergies]</p>";
-            $message .= "<p>Самое дальнее путешествие: $data[yourTrip]</p>";
+            $message = \models\Mail::createMessage($data);
             \models\FormAction::record($f3, $data);
             \models\Mail::mailTo($email, $subject, $message);
             $f3->status(200);
         } else {
             echo $validate[textError];
-            $f3->status(418);
+            $f3->status(400);
         }
     }
 }
--- autoload/models/Mail.php
+++ autoload/models/Mail.php
@@ -12,4 +12,22 @@ class Mail {
         
         return mail($email,$subject,$message,$header);
     }
+
+    public static function createMessage($data)
+    {
+        $message = "<p>ФИО: $data[fio]</p>";
+        $message .= "<p>Факультет: $data[fakultet]</p>";
+        $message .= "<p>Курс: $data[kurs]</p>";
+        $message .= "<p>Группа: $data[group]</p>";
+        $message .= "<p>Телефон: $data[phone]</p>";
+        $message .= "<p>Ссылка VK: $data[vkLink]</p>";
+        $message .= "<p>Опыт походов: $data[experience]</p>";
+        $message .= "<p>Другой полезный опыт: $data[otherExperience]</p>";
+        $message .= "<p>Имеющийся инвентарь: $data[inventoryAvailiable]</p>";
+        $message .= "<p>Уровень физической подготовки (1-10): $data[fitnessLevel]</p>";
+        $message .= "<p>Болезни и травмы: $data[diseases]</p>";
+        $message .= "<p>Аллергии: $data[allergies]</p>";
+        $message .= "<p>Самое дальнее путешествие: $data[yourTrip]</p>";
+        return $message;
+    }
 }
